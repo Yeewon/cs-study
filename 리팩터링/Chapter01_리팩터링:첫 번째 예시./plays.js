10af36a6e272e65af7202ae7e1b255ab8a09ec8e
Yeewon <yes3427@kookmin.ac.kr>
2022-02-21 02:05:21 +0900

    refactor: 함수 선언 바꾸기

--- 리팩터링/Chapter01_리팩터링:첫 번째 예시./plays.js
+++ 리팩터링/Chapter01_리팩터링:첫 번째 예시./plays.js
@@ -3,7 +3,7 @@ const statement = (invoice, plays) => {
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
-  const format = (aNumber) => {
+  const usd = (aNumber) => {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
@@ -51,12 +51,12 @@ const statement = (invoice, plays) => {
 
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
-    result += ` ${playFor(performance).name}: ${format(
-      amountFor(perf) / 100
-    )} (${perf.audience}석)\n`;
+    result += ` ${playFor(performance).name}: ${usd(amountFor(perf) / 100)} (${
+      perf.audience
+    }석)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 };
