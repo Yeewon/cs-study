0ef03fccb96ecfaa58d8dfe73dfde1d00dc6b906
Yeewon <yes3427@kookmin.ac.kr>
2022-02-21 02:00:07 +0900

    refactor: 명확한 이름으로 변경

--- 리팩터링/Chapter01_리팩터링:첫 번째 예시./plays.js
+++ 리팩터링/Chapter01_리팩터링:첫 번째 예시./plays.js
@@ -37,12 +37,12 @@ const statement = (invoice, plays) => {
     return result;
   };
 
-  const volumeCreditsFor = (perf) => {
+  const volumeCreditsFor = (aPerformance) => {
     let result = 0;
 
-    result += Math.max(perf.audience - 30, 0);
-    if ("comedy" === playFor(perf).type)
-      result += Math.floor(perf.audience / 5);
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(aPerformance).type)
+      result += Math.floor(aPerformance.audience / 5);
     return result;
   };
 
