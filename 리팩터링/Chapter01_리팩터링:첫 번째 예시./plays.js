5921f08cbd0c5b31e620c3081126e9e9001eaca0
Yeewon <yes3427@kookmin.ac.kr>
2022-02-21 02:07:42 +0900

    refactor: 단위 변환 로직 usd 함수로 이동

--- 리팩터링/Chapter01_리팩터링:첫 번째 예시./plays.js
+++ 리팩터링/Chapter01_리팩터링:첫 번째 예시./plays.js
@@ -8,7 +8,7 @@ const statement = (invoice, plays) => {
       style: "currency",
       currency: "USD",
       minimumFractionDigits: 2,
-    }).format(aNumber);
+    }).format(aNumber / 100);
   };
 
   const playFor = (aPerformance) => {
@@ -51,12 +51,12 @@ const statement = (invoice, plays) => {
 
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
-    result += ` ${playFor(performance).name}: ${usd(amountFor(perf) / 100)} (${
+    result += ` ${playFor(performance).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `총액: ${usd(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 };
