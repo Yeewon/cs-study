c845d1fd82051233baf59e9f70af7805f6dff056
Yeewon <yes3427@kookmin.ac.kr>
2022-02-21 15:27:15 +0900

    refactor: 두 파일로 명확하게 분리하기

--- 리팩터링/Chapter01_리팩터링:첫 번째 예시./plays.js
+++ 리팩터링/Chapter01_리팩터링:첫 번째 예시./plays.js
@@ -1,8 +1,4 @@
-const statement = (invoice, plays) => {
-  return renderHtml(createStatementData(invoice, plays));
-};
-
-const createStatementData = (invoice, plays) => {
+export const createStatementData = (invoice, plays) => {
   const statementData = {};
 
   statementData.customer = invoice.customer;
@@ -62,23 +58,4 @@ const createStatementData = (invoice, plays) => {
   };
 
   return statementData;
-};
-
-const renderHtml = (data) => {
-  let result = `청구 내역 (고객명: ${data.customer})\n`;
-  for (let perf of data.performances) {
-    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
-  }
-  result += `총액: ${usd(data.totalAmount)}\n`;
-  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
-
-  const usd = (aNumber) => {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
-  };
-
-  return result;
 };
