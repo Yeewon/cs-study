08b5aa1e80f27648d5cfcb4ec444cf3e5da46371
Yeewon <yes3427@kookmin.ac.kr>
2022-02-21 16:12:28 +0900

    refactor: volumeCredits 함수를 인라인

--- 리팩터링/Chapter01_리팩터링:첫 번째 예시./createStatementData.js
+++ 리팩터링/Chapter01_리팩터링:첫 번째 예시./createStatementData.js
@@ -31,9 +31,9 @@ class PerformanceCalculator {
   get volumeCredits() {
     let result = 0;
 
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
+    result += Math.max(this.performance.audience - 30, 0);
+    if ("comedy" === this.play.type)
+      result += Math.floor(this.performance.audience / 5);
     return result;
   }
 }
